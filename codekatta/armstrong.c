bb2f4b98949be2f28458db177468a4d24a8e1292
gdaravindhan09091997 <31499295+gdaravindhan09091997@users.noreply.github.com>
2018-01-21 11:12:30 +0530

    Create armstrong.c

---
+++ codekatta/armstrong.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+	int a,temp,r,sum=0;
+	printf("Enter the number:\n");
+	scanf("%d",&a);
+	temp=a;
+	while(a>0)
+	{
+r=a%10;
+sum=sum+r*r*r;
+a=a/10;
+	}
+	if(temp==sum)
+	printf("%d is an armstrong number\n",temp);
+	else
+	printf("%d is not an armstrong number",temp);
+	return 0;
+	
+}
