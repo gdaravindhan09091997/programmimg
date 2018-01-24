be5cdfcf9c5cced497887eebd662a980b90f4800
gdaravindhan09091997 <31499295+gdaravindhan09091997@users.noreply.github.com>
2018-01-24 20:25:46 +0530

    Create 21.c

---
+++ codekatta/21.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int a,n,d,sum=0,i;
+	scanf("%d",&a);
+	scanf("%d",&n);
+	scanf("%d",&d);
+	for(i=n;i<=a;i=i+d)
+	{
+		sum=sum+d*i;
+	}
+	printf("%d",sum);
+	
+	return 0;
+}
