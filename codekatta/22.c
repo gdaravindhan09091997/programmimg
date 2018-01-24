d5148175fd50d698a4bffa3c5bcec6abe4282348
gdaravindhan09091997 <31499295+gdaravindhan09091997@users.noreply.github.com>
2018-01-24 20:29:34 +0530

    Create 22.c

---
+++ codekatta/22.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+int i,n,max,a[10];
+scanf("%d",&m);
+for(i=0;i<=n;i++)
+{
+	scanf("%d",&a[i]);
+}
+max=a[0];
+for(i=0;i<n;i++)
+
+if(max<a[i])
+max=a[i];
+{
+ printf("%d",max);
+}
+}
+return 0;
+}
