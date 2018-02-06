7295470d8f050d31ac95f6b37575d3c228622f45
gdaravindhan09091997 <31499295+gdaravindhan09091997@users.noreply.github.com>
2018-02-06 21:06:45 +0530

    Update 23.c

--- codekatta/23.c
+++ codekatta/23.c
@@ -1,16 +1,22 @@
 #include<stdio.h>
 int main()
 {
-	int c[50],n,i,l;
-    scanf("%d",&n);
-    for(i=0;i<n;i++)
-        scanf("%d",&c[i]);
-    s=c[0];
-    for (i=1;i<n;i++)
-    {
-        if(s>c[i])
-            s= c[i];
-    }
-    printf("%d",s);
-    return 0;
+	int a[100],n,i,smaller;
+	printf("Enter the number of elements in the array\n");
+	scanf("%d",&n);
+	printf("Enter the elements\n");
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	smaller=a[0];
+for(i=0;i<n;i++)
+{
+	if(a[i]<smaller)
+	{
+		smaller=a[i];
+	}
+}
+printf("The smallest element in the array is %d",smaller);
+return 0;
 }
