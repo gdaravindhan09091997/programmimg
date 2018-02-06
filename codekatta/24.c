853b0be14a8ecde31d31a0d1fd0632e5a6f49379
gdaravindhan09091997 <31499295+gdaravindhan09091997@users.noreply.github.com>
2018-02-06 21:42:54 +0530

    Update 24.c

--- codekatta/24.c
+++ codekatta/24.c
@@ -1,22 +1,31 @@
 #include<stdio.h>
 int main()
 {
-int i,j, a[90],m,temp;
-printf("enter no ");
-scanf("%d",&m);
-printf("enter array");
-for(i=1;i<=m;i++)
-scanf("%d",&a[i]);
-for(i=1;i<=m;i++)
-{
-for(j=i+1;j<=m;j++)
-{
-temp=a[i];
-a[i]=a[j];
-a[j]=temp;
+	int n,i,a[n],temp,j;
+	printf("Enter the number of elements in the array\n");
+	scanf("%d",&n);
+	printf("Enter the array elements\n");
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	for(i=0;i<n;i++)
+	{
+		for(j=i+1;j<n;j++)
+		{
+			if(a[i]>a[j])
+			{
+				temp=a[i];
+				a[i]=a[j];
+				a[j]=temp;
+			}
+		}
+	}
+	printf("The sorted array is\n");
+	for(i=0;i<n;i++)
+	{
+	
+	printf("%d\n",a[i]);
 }
-}
-for(i=1;i<=m;i++)
-printf("%d ",a[i]);
 return 0;
 }
