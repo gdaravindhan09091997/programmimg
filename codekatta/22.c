6774b7afae8155e1dbe56b54bcbeac897d99042b
gdaravindhan09091997 <31499295+gdaravindhan09091997@users.noreply.github.com>
2018-02-06 21:02:46 +0530

    Update 22.c

--- codekatta/22.c
+++ codekatta/22.c
@@ -1,21 +1,24 @@
-#include <stdio.h>
-
+#include<stdio.h>
 int main()
 {
-int i,n,max,a[10];
-scanf("%d",&m);
-for(i=0;i<=n;i++)
-{
-	scanf("%d",&a[i]);
-}
-max=a[0];
+	int a[100],n,i,greater;
+	printf("Enter the number of elements in the array\n");
+	scanf("%d",&n);
+	printf("Enter the elements\n");
+	for(i=0;i<n;i++)
+	{
+		
+	
+		scanf("%d",&a[i]);
+	}
+	greater=a[0];
 for(i=0;i<n;i++)
-
-if(max<a[i])
-max=a[i];
 {
- printf("%d",max);
+	if(a[i]>greater)
+	{
+		greater=a[i];
+	}
 }
-}
+printf("The largest element in the array is %d",greater);
 return 0;
 }
