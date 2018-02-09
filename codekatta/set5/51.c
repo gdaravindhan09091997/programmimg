c7eb448bb7d9ac9f3c1684ade3aedce38ca6a119
gdaravindhan09091997 <31499295+gdaravindhan09091997@users.noreply.github.com>
2018-02-09 15:41:17 +0530

    Create 51.c

---
+++ codekatta/set5/51.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,r=0;
+	printf("Enter the number");
+	scanf("%d",&a);
+	while(a!=0)
+	{
+		b=a%10;
+		r=r*10+b;
+		a=a/10;
+	}
+	a=r;
+	while(a!=0)
+	{
+		b=a%10;
+		printf("%d ",b);
+		a=a/10;
+	}
+	return 0;
+}
