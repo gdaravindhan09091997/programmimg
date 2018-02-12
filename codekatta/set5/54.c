de466bb01d9426ae75499886063cf3c304a461eb
gdaravindhan09091997 <31499295+gdaravindhan09091997@users.noreply.github.com>
2018-02-12 15:48:32 +0530

    Create 54.c

---
+++ codekatta/set5/54.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int a;
+	printf("Enter the number ");
+	scanf("%d",&a);
+	if(a%2==0)
+	printf("%d",a);
+	else
+	printf("%d",--a);
+	return 0;
+}
+Success	#stdin #std
